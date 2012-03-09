6e30f43941654d86b94256a753e563e392e7dadf
stephen.more@gmail.com <stephen.more@gmail.com@9135dbcb-0826-04c1-d3ba-6611f014da28>
2012-03-09 02:13:50 +0000

    Trying to get a simple MyFaces CODI example working.

---
+++ jsf-codi/src/main/java/org/test/Count.java
@@ -0,0 +1,39 @@
+package org.test;
+
+import java.io.Serializable;
+import javax.inject.Named;
+//import org.apache.myfaces.extensions.cdi.core.api.scope.conversation.WindowScoped;
+import org.apache.myfaces.extensions.cdi.core.api.scope.conversation.ConversationScoped;
+
+@Named
+//@WindowScoped
+@ConversationScoped
+public class Count implements Serializable {
+
+    private Integer count = 0;
+
+    public Count() {
+	System.out.println( "init" );
+    }
+
+    public Integer getCount() {
+	return count;
+    }
+
+    public void setCount( Integer i ) {
+ 	count = i;
+    }
+
+    public String add() {
+	System.out.println( "adding" );
+        count++;
+
+	return "home";
+    }
+
+    public String reset() {
+        count = 0;
+
+	return "home";
+    }
+}
