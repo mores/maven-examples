c2311bb205514e7997bd1c14c7353d8d5d2fcf9b
stephen.more@gmail.com <stephen.more@gmail.com@9135dbcb-0826-04c1-d3ba-6611f014da28>
2012-10-31 16:50:17 +0000

    Now works with HttpsUrlConnectionMessageSender.

--- spring-client-cert/src/test/java/org/test/AppTest.java
+++ spring-client-cert/src/test/java/org/test/AppTest.java
@@ -24,6 +24,7 @@ public class AppTest extends TestCase
 	{
 		org.springframework.context.ApplicationContext context = new org.springframework.context.support.FileSystemXmlApplicationContext( "classpath:cert-beans.xml" );
 		java.security.KeyStore keyStore = (java.security.KeyStore)context.getBean("keyStore");
-		javax.net.ssl.KeyManagerFactory keyManager = (javax.net.ssl.KeyManagerFactory)context.getBean("keyManager");
+		javax.net.ssl.KeyManager[] keyManager = (javax.net.ssl.KeyManager[])context.getBean("keyManager");
+		org.springframework.ws.transport.http.HttpsUrlConnectionMessageSender sender = (org.springframework.ws.transport.http.HttpsUrlConnectionMessageSender)context.getBean("sender");
 	}
 }
