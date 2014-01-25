81f4fc871dc39a72245defb1e040e1bfa3f13293
stephen.more@gmail.com <stephen.more@gmail.com@9135dbcb-0826-04c1-d3ba-6611f014da28>
2014-01-25 03:52:44 +0000

    Simple Add Native Lib

---
+++ trunk/robovm/native/src/xcode/robovm-native/robovm_native.m
@@ -0,0 +1,15 @@
+//
+//  robovm_native.m
+//  robovm-native
+//
+//  Created by Stephen More on 1/24/14.
+//  Copyright (c) 2014 HelloWorld. All rights reserved.
+//
+
+#import "robovm_native.h"
+
+@implementation robovm_native
+-(int)add:(int)a :(int)b {
+    return a + b;
+}
+@end
