126de2c9f8dd0f200fb3035a0162cae9e75c2008
stephen.more@gmail.com <stephen.more@gmail.com@9135dbcb-0826-04c1-d3ba-6611f014da28>
2014-01-25 03:52:44 +0000

    Simple Add Native Lib

---
+++ robovm/native/src/xcode/robovm-native/robovm_native.h
@@ -0,0 +1,13 @@
+//
+//  robovm_native.h
+//  robovm-native
+//
+//  Created by Stephen More on 1/24/14.
+//  Copyright (c) 2014 HelloWorld. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface robovm_native : NSObject
+-(int)add:(int)a :(int)b;
+@end
---
+++ robovm/native/src/xcode/robovm-nativeTests/robovm_nativeTests.m
@@ -0,0 +1,34 @@
+//
+//  robovm_nativeTests.m
+//  robovm-nativeTests
+//
+//  Created by Stephen More on 1/24/14.
+//  Copyright (c) 2014 HelloWorld. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface robovm_nativeTests : XCTestCase
+
+@end
+
+@implementation robovm_nativeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
