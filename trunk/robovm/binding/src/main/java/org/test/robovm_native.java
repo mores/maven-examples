d548e86f62f72e8764e4ed43c0eb2c5f0c014016
stephen.more@gmail.com <stephen.more@gmail.com@9135dbcb-0826-04c1-d3ba-6611f014da28>
2014-02-15 12:14:24 +0000

    Bindings now working for three methods

--- trunk/robovm/binding/src/main/java/org/test/robovm_native.java
+++ trunk/robovm/binding/src/main/java/org/test/robovm_native.java
@@ -16,18 +16,29 @@ public class robovm_native extends NSObject {
         ObjCRuntime.bind(robovm_native.class);
     }
 
-    private static final Selector add = Selector.register("add");
-    @Bridge
-    private native static int objc_add(robovm_native __self__, Selector __cmd__, int a, int b);
-
-    public int add(int a, int b) {
-        return objc_add(this, add, a, b);
-    }
-
+    // No parameters so no colon
     private static final Selector displayAlert = Selector.register("displayAlert");
     @Bridge
     private native static void objc_displayAlert(robovm_native __self__, Selector __cmd__);
     public void displayAlert() {
         objc_displayAlert(this, displayAlert);
+    }
+
+    // Only 1 parameter so just 1 colon
+    private static final Selector doubleUp = Selector.register("doubleUp:");
+    @Bridge
+    private native static int objc_doubleUp(robovm_native __self__, Selector __cmd__, int a );
+
+    public int doubleUp(int a) {
+        return objc_doubleUp(this, doubleUp, a );
+    }
+
+    // Two parameters so add another :
+    private static final Selector add = Selector.register("add::");
+    @Bridge
+    private native static int objc_add(robovm_native __self__, Selector __cmd__, int a, int b);
+
+    public int add(int a, int b) {
+        return objc_add(this, add, a, b);
     }
 }
