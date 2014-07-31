9de3552a0cf5241434267053db59db5065ca8cca
stephen.more@gmail.com <stephen.more@gmail.com@9135dbcb-0826-04c1-d3ba-6611f014da28>
2014-07-31 23:15:17 +0000

    Adding TemperatureGauge.

---
+++ trunk/mvc-TemperatureConvert/src/main/java/edu/pace/csis/bergin/TemperatureGauge.java
@@ -0,0 +1,13 @@
+package edu.pace.csis.bergin;
+
+class TemperatureGauge
+{	
+	public TemperatureGauge(int min, int max){ Min = min; Max = max; }
+	
+	public void set(int level) { current = level; }	
+	public int get(){return current;}
+	public int getMax(){return Max;}
+	public int getMin(){return Min;}
+	
+	private int Max, Min, current;
+}
