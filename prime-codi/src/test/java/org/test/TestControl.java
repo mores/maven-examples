2bdfefed7a670bbade29ed05c2a906213eccb5a2
Stephen More <smore@smartwattinc.com>
2016-07-05 11:16:32 -0400

    Adding a TestControl Test.

---
+++ prime-codi/src/test/java/org/test/TestControl.java
@@ -0,0 +1,35 @@
+package org.test;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.assertTrue;
+import org.apache.deltaspike.testcontrol.api.junit.CdiTestRunner;
+
+@RunWith(CdiTestRunner.class)
+public class TestControl {
+
+  @Inject
+  Multiplicand multiplicand;
+
+  @Inject
+  Multiplier multiplier;
+ 
+  @Inject
+  Product product;
+ 
+  @Test
+  public void testMultiply() {
+
+    multiplicand.setValue( new java.math.BigDecimal( "3" ) );
+
+    multiplier.setValue( new java.math.BigDecimal( "7" ) );
+
+    java.math.BigDecimal value = product.getValue(); 
+    System.out.println( "value: " + value );
+
+    assertTrue( value.compareTo( new java.math.BigDecimal( "21" ) ) == 0 );
+
+  }
+}
