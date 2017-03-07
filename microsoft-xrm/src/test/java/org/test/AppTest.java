b127b55c3989f88770381143ba7aff82814477bd
Stephen More <smore@smartwattinc.com>
2017-03-07 16:20:28 -0500

    Reads And Writes to Xrm.

--- microsoft-xrm/src/test/java/org/test/AppTest.java
+++ microsoft-xrm/src/test/java/org/test/AppTest.java
@@ -28,32 +28,51 @@ public class AppTest
 		org.apache.http.impl.client.HttpClientBuilder clientBuilder = org.apache.http.impl.client.HttpClientBuilder.create();
 		org.apache.http.impl.client.CloseableHttpClient httpClient = clientBuilder.build();
 
-		org.apache.http.client.methods.HttpGet httpget = new org.apache.http.client.methods.HttpGet( "https://" + company + ".crm.dynamics.com/api/data/v8.1/accounts/" );
+		String url = "https://" + company + ".crm.dynamics.com/api/data/v8.1/accounts/";
+	
+		org.apache.http.client.methods.HttpPost httppost = new org.apache.http.client.methods.HttpPost( url );
+		httppost.addHeader( "Content-Type", "application/json; charset=utf-8" );
+		httppost.addHeader( "Accept", "application/json" );
+                httppost.addHeader( "Authorization", "Bearer " + result.getAccessToken() );
+                httppost.addHeader( "Host", company + ".crm.dynamics.com" );
+
+		String newAccount = "{ 'name': 'Sample Account', 'creditonhold': false, 'address1_latitude': 47.639583, 'description': 'This is the description of the sample account', 'revenue': 5000000, 'accountcategorycode': 1 }";
+		httppost.setEntity( new org.apache.http.entity.StringEntity( newAccount, "utf-8" ) );
+
+		org.apache.http.HttpResponse response = httpClient.execute( httppost );
+		org.apache.http.StatusLine sl = response.getStatusLine();
+                log.info( "code: " + sl.getStatusCode() );
+		org.apache.http.Header[] headers = response.getHeaders( "Location" );
+		for( org.apache.http.Header header : headers)
+		{
+			log.info( "Added Account: " + header.getValue() );
+		}
+
+		org.apache.http.client.methods.HttpGet httpget = new org.apache.http.client.methods.HttpGet( url );
 		httpget.addHeader( "Accept", "application/json" );
 		httpget.addHeader( "Authorization", "Bearer " + result.getAccessToken() );
 		httpget.addHeader( "Host", company + ".crm.dynamics.com" );
 		httpget.addHeader( "OData-Version", "4.0" );
 		httpget.addHeader( "Cache-Control", "no-cache" );
-		org.apache.http.HttpResponse response = httpClient.execute( httpget );
-		org.apache.http.StatusLine sl = response.getStatusLine();
-		log.info( "code: " + sl.getStatusCode() );
-		if( sl.getStatusCode() == 200 )
+		org.apache.http.HttpResponse getResponse = httpClient.execute( httpget );
+		org.apache.http.StatusLine getStatus = getResponse.getStatusLine();
+		log.info( "code: " + getStatus.getStatusCode() );
+		if( getStatus.getStatusCode() == 200 )
 		{
-			org.apache.http.HttpEntity entity = response.getEntity();
-			StringBuilder sb = new StringBuilder();
-			java.io.BufferedReader br = new java.io.BufferedReader( new java.io.InputStreamReader( entity.getContent() ) );
-			String line = null;
+			org.apache.http.HttpEntity entity = getResponse.getEntity();
 
-                        while( ( line = br.readLine() ) != null )
-                        {
-                                sb.append( line );
-                        }
+			java.io.BufferedInputStream bis = new java.io.BufferedInputStream(entity.getContent());
+			String filePath = "data.json";
+			java.io.BufferedOutputStream bos = new java.io.BufferedOutputStream(new java.io.FileOutputStream(new java.io.File(filePath)));
+			int inByte;
+			while((inByte = bis.read()) != -1)
+			{
+				bos.write(inByte);
+			}
+			bis.close();
+			bos.close();
 
-                        br.close();
-
-                        org.apache.http.util.EntityUtils.consume( entity );
-
-			log.debug( sb.toString() );
+			log.info( "File saved: " + filePath );
 		}
 	}
 }
