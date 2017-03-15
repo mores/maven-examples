a14bcfab8b6f5f7ba97d1e555cb27e59f266425a
Stephen More <smore@smartwattinc.com>
2017-03-14 20:44:00 -0400

    Adds FacesMessage to test case.

--- prime-deltaspike/src/main/java/org/test/Product.java
+++ prime-deltaspike/src/main/java/org/test/Product.java
@@ -24,6 +24,15 @@ public class Product implements Serializable {
 		java.math.BigDecimal value = null;
 		try {
 			value = multiplier.getValue().multiply(multiplicand.getValue());
+
+			if (value.compareTo(java.math.BigDecimal.valueOf(500)) > 0) {
+				System.out.println("Over limit");
+				javax.faces.application.FacesMessage errorMsg = new javax.faces.application.FacesMessage(
+						"Over limit. Management approval required");
+				javax.faces.context.FacesContext.getCurrentInstance()
+						.addMessage(null, errorMsg);
+			}
+
 		} catch (Exception e) {
 			System.out.println("Error: " + e);
 		}
