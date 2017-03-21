304382e2f9587a5b18431e284a9e86f192d520d3
mores <stephen.more@gmail.com>
2017-03-20 17:03:04 -0700

    Adds a FacesTest utilizing myFaces.

--- prime-deltaspike/src/test/java/org/test/TestMultiplication.java
+++ prime-deltaspike/src/test/java/org/test/TestMultiplication.java
@@ -35,14 +35,18 @@ public class TestMultiplication {
 		contextControl.startContext(RequestScoped.class);
 		windowContext.activateWindow("testWindow");
 
-		multiplicand.setValue(new java.math.BigDecimal("3"));
+		multiplicand.setValue(new java.math.BigDecimal("30"));
 
-		multiplier.setValue(new java.math.BigDecimal("7"));
+		multiplier.setValue(new java.math.BigDecimal("70"));
 
 		java.math.BigDecimal value = product.getValue();
 		System.out.println("value: " + value);
 
-		Assert.assertTrue(value.compareTo(new java.math.BigDecimal("21")) == 0);
+		Assert.assertTrue(value.compareTo(new java.math.BigDecimal("2100")) == 0);
+
+		Assert.assertEquals("Over limit. Management approval required",
+				javax.faces.context.FacesContext.getCurrentInstance()
+						.getMessageList().iterator().next().getSummary());
 
 		contextControl.stopContext(RequestScoped.class);
 
