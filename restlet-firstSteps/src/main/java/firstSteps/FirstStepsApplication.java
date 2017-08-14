d519791f4d69cef3130d5b7305b834d10c4e4517
Stephen More <smore@smartwattinc.com>
2017-08-14 14:57:04 -0400

    Now supports swagger 2.0 spec.

--- restlet-firstSteps/src/main/java/firstSteps/FirstStepsApplication.java
+++ restlet-firstSteps/src/main/java/firstSteps/FirstStepsApplication.java
@@ -1,6 +1,6 @@
 package firstSteps;  
   
-public class FirstStepsApplication extends org.restlet.ext.swagger.SwaggerApplication {
+public class FirstStepsApplication extends org.restlet.Application {
   
     /** 
      * Creates a root Restlet that will receive all incoming calls. 
@@ -10,6 +10,15 @@ public class FirstStepsApplication extends org.restlet.ext.swagger.SwaggerApplic
         // Create a router Restlet that routes each call to a  
         // new instance of HelloWorldResource.  
         org.restlet.routing.Router router = new org.restlet.routing.Router(getContext());  
+
+	org.restlet.ext.swagger.Swagger2SpecificationRestlet swagger2SpecificationRestlet = new org.restlet.ext.swagger.Swagger2SpecificationRestlet(this);
+	try {
+		String hostName = java.net.InetAddress.getLocalHost().getHostName();
+		swagger2SpecificationRestlet.setBasePath( "http://" + hostName + ":9090/firstSteps/" );
+	} catch ( Exception e ) {
+		e.printStackTrace();
+	}
+	swagger2SpecificationRestlet.attach(router,"/api-docs");
   
         // Defines only one route  
         router.attach("/hello", HelloWorldResource.class);  
