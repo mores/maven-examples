fa375589aee9649f009e1363dfca7f83bf049ade
Stephen More <smore@smartwattinc.com>
2017-11-23 10:04:58 -0500

    Adds some simple logging.

--- azure-functions/README.txt
+++ azure-functions/README.txt
@@ -3,7 +3,7 @@ curl -L https://aka.ms/InstallAzureCli | bash
 mvn archetype:generate -B -DarchetypeGroupId=com.microsoft.azure -DarchetypeArtifactId=azure-functions-archetype -DarchetypeVersion=1.1 -DgroupId=org.test -DartifactId=hello -Dversion=1.0-SNAPSHOT -Dpackage=org.test.hello
 
 
-mvn clean package
+mvn -DskipTests clean package
 
 az login
 
--- azure-functions/hello/src/main/java/org/test/hello/Function.java
+++ azure-functions/hello/src/main/java/org/test/hello/Function.java
@@ -1,15 +1,20 @@
 package org.test.hello;
 
 import com.microsoft.azure.serverless.functions.annotation.*;
-import com.microsoft.azure.serverless.functions.ExecutionContext;
 
 /**
  * Hello function with HTTP Trigger.
  */
-public class Function {
-    @FunctionName("hello")
-    public String hello(@HttpTrigger(name = "req", methods = {"get", "post"}, authLevel = AuthorizationLevel.ANONYMOUS) String req,
-                        ExecutionContext context) {
-        return String.format("Hello, %s!", req);
-    }
+public class Function
+{
+	@FunctionName("hello")
+	public String hello( @HttpTrigger(name = "req", methods = {"get", "post"}, authLevel = AuthorizationLevel.ANONYMOUS) String req, com.microsoft.azure.serverless.functions.ExecutionContext context )
+	{
+		String functionName = context.getFunctionName();
+		String invocationId = context.getInvocationId();
+
+		context.getLogger().warning( functionName + "\t" + invocationId + "\t" + "said Hello." );
+
+		return String.format("{%s} {%s} Hello, %s!", functionName, invocationId, req);
+	}
 }
