fc71c1dbcbfe795f7e3f052d2262fb73d620c5cc
mores <stephen.more@gmail.com>
2018-01-16 05:14:02 -0800

    Adds example with swarm and swagger.

---
+++ swarm-swagger/README.txt
@@ -0,0 +1,17 @@
+mvn wildfly-swarm:run
+
+or
+
+mvn clean package
+
+---- then ---
+
+UI
+http://127.0.0.1:8080/swagger-ui/
+
+Descriptor
+http://127.0.0.1:8080/swagger.json
+
+Actual REST
+http://127.0.0.1:8080/time/now
+
---
+++ swarm-swagger/src/main/java/com/example/TimeResource.java
@@ -0,0 +1,57 @@
+package com.example;
+
+/*
+ * For example see: https://github.com/swagger-api/swagger-core/blob/master/modules/swagger-servlet/src/test/java/io/swagger/servlet/resources/ResourceWithAnnotations.java
+ */
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Context;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiKeyAuthDefinition;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Authorization;
+import io.swagger.annotations.BasicAuthDefinition;
+import io.swagger.annotations.Info;
+import io.swagger.annotations.OAuth2Definition;
+import io.swagger.annotations.SecurityDefinition;
+import io.swagger.annotations.SwaggerDefinition;
+import org.joda.time.DateTime;
+
+@Path( "/time" )
+@Api( value = "/time", description = "Get the time", tags = "time" )
+@Produces( MediaType.APPLICATION_JSON )
+@SwaggerDefinition(
+        info = @Info(
+                title = "Time API",
+                version = "so alpha"
+        ),
+        securityDefinition = @SecurityDefinition(
+                apiKeyAuthDefinitions = {
+                        @ApiKeyAuthDefinition( key = "api_key", name = "api_key", in = ApiKeyAuthDefinition.ApiKeyLocation.HEADER ) }
+        ) )
+public class TimeResource
+{
+	private static org.jboss.logging.Logger log = org.jboss.logging.Logger.getLogger( TimeResource.class );
+
+	@GET
+	@Secured
+	@Path( "/now" )
+	@ApiOperation( value = "Get the current time",
+	        notes = "Returns the time as a string",
+	        response = String.class,
+	        authorizations = @Authorization( value = "api_key" )
+	        )
+	@Produces( MediaType.APPLICATION_JSON )
+	public String get( @Context javax.ws.rs.core.SecurityContext securityContext )
+	{
+		log.info( "get" );
+
+		String userName = securityContext.getUserPrincipal().getName();
+
+		return String.format( "{\"value\" : \"" + userName + ", The time is %s\"}", new DateTime() );
+	}
+}
