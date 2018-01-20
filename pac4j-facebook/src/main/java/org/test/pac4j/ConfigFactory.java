02b5edbc9114c772f07a594eab7012a981a406d1
Stephen More <stephen.more@gmail.com>
2018-01-20 22:55:15 +0000

    Begining of facebook client.

---
+++ pac4j-facebook/src/main/java/org/test/pac4j/ConfigFactory.java
@@ -0,0 +1,25 @@
+package org.test.pac4j;
+
+public class ConfigFactory implements org.pac4j.core.config.ConfigFactory
+{
+	private String localCallbackUrl = "";
+
+	@Override
+	public org.pac4j.core.config.Config build( final Object... parameters )
+	{
+		String key = "";
+		String secret = "";
+
+		org.pac4j.oauth.client.FacebookClient facebookClient = new org.pac4j.oauth.client.FacebookClient( key, secret );
+
+		final org.pac4j.core.client.Clients clients = new org.pac4j.core.client.Clients( localCallbackUrl, facebookClient );
+		clients.setDefaultClient( facebookClient );
+
+		final org.pac4j.core.config.Config config = new org.pac4j.core.config.Config( clients );
+
+		org.pac4j.core.matching.PathMatcher pathMatcher = new org.pac4j.core.matching.PathMatcher();
+		config.addMatcher( "excludedPath", pathMatcher );
+	
+		return config;	
+	}
+}
---
+++ pac4j-facebook/src/main/java/org/test/pac4j/Tokens.java
@@ -0,0 +1,90 @@
+package org.test.pac4j;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean( name = "tokens" )
+@ViewScoped
+public class Tokens
+{
+	private static org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger( Tokens.class );
+
+	private org.pac4j.oauth.profile.facebook.FacebookProfile facebookProfile;
+	private String text;
+
+	public Tokens()
+	{
+		javax.faces.context.FacesContext facesContext = javax.faces.context.FacesContext.getCurrentInstance();
+		javax.faces.context.ExternalContext ectx = facesContext.getExternalContext();
+		javax.servlet.http.HttpServletRequest request = (javax.servlet.http.HttpServletRequest)ectx.getRequest();
+		javax.servlet.http.HttpServletResponse response = (javax.servlet.http.HttpServletResponse)ectx.getResponse();
+
+		org.pac4j.core.context.WebContext context = new org.pac4j.core.context.J2EContext( request, response );
+		org.pac4j.core.profile.ProfileManager<org.pac4j.oauth.profile.facebook.FacebookProfile> profileManager = new org.pac4j.core.profile.ProfileManager<org.pac4j.oauth.profile.facebook.FacebookProfile>( context );
+
+		java.util.List<org.pac4j.oauth.profile.facebook.FacebookProfile> profiles = profileManager.getAll( true );
+		facebookProfile = profiles.get( 0 );
+	}
+
+	/*
+	public String getIdToken()
+	{
+		com.nimbusds.jwt.JWT jwtFromOpenId = azureAdProfile.getIdToken();
+
+		try
+		{
+			com.nimbusds.jwt.JWTClaimsSet claims = jwtFromOpenId.getJWTClaimsSet();
+			java.util.Date expiresOn = claims.getExpirationTime();
+			log.info( "Expires on: " + expiresOn );
+
+			for( String claim : claims.getClaims().keySet() )
+			{
+				log.info( claim + "\t" + claims.getClaims().get( claim ) );
+			}
+		}
+		catch ( Exception e )
+		{
+			e.printStackTrace();
+		}
+
+		return jwtFromOpenId.serialize();
+	}
+
+	public String getAccessToken()
+	{
+		return azureAdProfile.getAccessToken().getValue();
+	}
+
+	public java.util.Date getAuthTime()
+	{
+		return azureAdProfile.getAuthTime();
+	}
+
+	public String getRefreshToken()
+	{
+		com.nimbusds.oauth2.sdk.token.RefreshToken refreshToken = azureAdProfile.getRefreshToken();
+		return refreshToken.getValue();
+	}
+
+	public String getAccessTokenFromRefreshToken()
+	{
+		org.pac4j.core.config.Config config = org.pac4j.core.config.ConfigSingleton.getConfig();
+		org.pac4j.core.client.Clients clients = config.getClients();
+		org.pac4j.oidc.client.AzureAdClient azureAdClient = clients.findClient( org.pac4j.oidc.client.AzureAdClient.class );
+		String accessToken = azureAdClient.getAccessTokenFromRefreshToken( azureAdProfile );
+
+		log.info( "obj: " + azureAdClient );
+		return azureAdClient.getAccessTokenFromRefreshToken( azureAdProfile );
+	}
+
+	public String getText()
+	{
+		return text;
+	}
+
+        public void setText(String text)
+	{
+        	this.text = text;
+	}
+	*/
+}
