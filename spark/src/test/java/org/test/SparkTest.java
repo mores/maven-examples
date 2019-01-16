5877040407a9d1a815f3c4f46ba330a610ce777a
Stephen More <stephen.more@gmail.com>
2019-01-16 15:08:16 -0500

    Now reads in a json file.

--- spark/src/test/java/org/test/SparkTest.java
+++ spark/src/test/java/org/test/SparkTest.java
@@ -11,14 +11,34 @@ public class SparkTest
 	{
 		log.debug( "testOne" );
 
-		String testFile = "src/test/resources/README.md";
 		org.apache.spark.sql.SparkSession spark = org.apache.spark.sql.SparkSession.builder().appName("Simple Application").config("spark.master", "local").getOrCreate();
-		org.apache.spark.sql.Dataset<String> logData = spark.read().textFile( testFile ).cache();
+		org.apache.spark.sql.Dataset<String> logData = spark.read().textFile( "src/test/resources/README.md" ).cache();
 
 		long numAs = logData.filter(s -> s.contains("a")).count();
 		long numBs = logData.filter(s -> s.contains("b")).count();
 
 		log.debug( "Lines with a: " + numAs + ", lines with b: " + numBs );
+
+		org.junit.Assert.assertEquals( 62L, numAs );
+		org.junit.Assert.assertEquals( 31L, numBs );
+
+		org.apache.spark.sql.Dataset<org.apache.spark.sql.Row> people = spark.read().json( "src/test/resources/people.json").cache();
+		people.printSchema();
+
+		org.apache.spark.sql.Dataset<org.apache.spark.sql.Row> cooked = people.agg( org.apache.spark.sql.functions.avg( people.col( "age" ) ).as( "averageAge" ), org.apache.spark.sql.functions.count( people.col( "name" ) ) );
+		cooked.printSchema();
+
+		org.apache.spark.sql.Row row = cooked.first();
+		double average = row.getDouble( 0 );
+		long count = row.getLong( 1 );
+
+		log.debug( "average: " + average );
+		log.debug( "count: " + count );
+
+		org.junit.Assert.assertEquals( (double)24.5, average, 0.001 );
+		org.junit.Assert.assertEquals( 3, count );
+
+		cooked.show();
 
 		spark.stop();
 	}
