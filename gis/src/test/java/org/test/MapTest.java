b268d2983bfd0c28e386445601593200e5da05f3
Stephen More <stephen.more@gmail.com>
2019-11-25 10:09:10 -0500

    Now able to show the center of the points.

--- gis/src/test/java/org/test/MapTest.java
+++ gis/src/test/java/org/test/MapTest.java
@@ -20,8 +20,15 @@ public class MapTest {
 
 	private static org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(MapTest.class);
 
+	private static org.locationtech.jts.geom.GeometryFactory gf;
+
+	private static java.util.List<org.geotools.process.spatialstatistics.core.SpatialEvent> events;
+
 	@Test
 	public void test() throws Exception {
+
+		gf = new org.locationtech.jts.geom.GeometryFactory();
+		events = new java.util.ArrayList<>();
 
 		Filter filter = CQL.toFilter("STATE = 'NY'");
 
@@ -46,26 +53,40 @@ public class MapTest {
 		simpleFeatureTypeBuilder.setCRS(org.geotools.referencing.crs.DefaultGeographicCRS.WGS84);
 		simpleFeatureTypeBuilder.add("the_geom",org.locationtech.jts.geom.Point.class);
 		simpleFeatureTypeBuilder.length(15).add("Name", String.class);
-		org.opengis.feature.simple.SimpleFeatureType LOCATION = simpleFeatureTypeBuilder.buildFeatureType();
-
-		org.locationtech.jts.geom.GeometryFactory gf = new org.locationtech.jts.geom.GeometryFactory();
-		org.locationtech.jts.geom.Point point = gf.createPoint(new org.locationtech.jts.geom.Coordinate(-73.8807208f, 42.6680631f));
-
-		org.geotools.feature.simple.SimpleFeatureBuilder simpleFeatureBuilder = new org.geotools.feature.simple.SimpleFeatureBuilder( LOCATION);
-		simpleFeatureBuilder.add(point);
-		simpleFeatureBuilder.add("Albany");
-		String id = "101";
-		org.opengis.feature.simple.SimpleFeature simpleFeature = simpleFeatureBuilder.buildFeature(id);
+		org.opengis.feature.simple.SimpleFeatureType CITY = simpleFeatureTypeBuilder.buildFeatureType();
 
 		org.geotools.feature.DefaultFeatureCollection featureCollection = new org.geotools.feature.DefaultFeatureCollection();
-		featureCollection.add(simpleFeature);
+
+		featureCollection.add( addPoint( CITY, -73.8807208, 42.6680631, "Albany" ) );
+		featureCollection.add( addPoint( CITY, -78.9346538, 42.8962175, "Buffalo" ) );
+		featureCollection.add( addPoint( CITY, -74.2605513, 40.6971478, "New York City" ) );
 
 		log.trace("Point Bounds: " + featureCollection.getBounds());
 
-		Layer layer2 = new FeatureLayer(featureCollection, SLD.createPointStyle("circle", java.awt.Color.RED, java.awt.Color.GREEN, 1.0f, 1.0f));
+		Layer layer2 = new FeatureLayer(featureCollection, SLD.createPointStyle("circle", java.awt.Color.RED, java.awt.Color.GREEN, 1.0f, 15.0f));
 		layer2.setVisible(true);
 		layer2.setTitle("Points");
 		map.addLayer(layer2);
+
+		org.geotools.process.spatialstatistics.core.DistanceFactory distanceFactory = new org.geotools.process.spatialstatistics.core.DistanceFactory();
+		double standardDeviation = 1.0;
+		boolean useWeight = false;
+		org.geotools.process.spatialstatistics.core.SpatialEvent center = distanceFactory.getStandardDistance( events, standardDeviation, useWeight );
+		log.info( "Center: " + center.getCoordinate() );
+
+		for( org.geotools.process.spatialstatistics.core.SpatialEvent event : events )
+		{
+			double distance = distanceFactory.getDistance( center, event );
+			log.trace( "Distance: " + distance );
+		}
+
+		org.geotools.feature.DefaultFeatureCollection centerCollection = new org.geotools.feature.DefaultFeatureCollection();
+		centerCollection.add( addPoint( CITY, center.getCoordinate().getX(), center.getCoordinate().getY(), "Center" ) );
+
+		Layer layer3 = new FeatureLayer( centerCollection, SLD.createPointStyle("star", java.awt.Color.YELLOW, java.awt.Color.RED, 1.0f, 15.0f));
+		layer3.setVisible(true);
+		layer3.setTitle("Center");
+		map.addLayer(layer3);
 
 		org.geotools.renderer.GTRenderer renderer = new org.geotools.renderer.lite.StreamingRenderer();
 		renderer.setMapContent(map);
@@ -85,6 +106,21 @@ public class MapTest {
 
 		java.io.File file = new java.io.File("/tmp/out.jpg");
 		javax.imageio.ImageIO.write(image, "jpeg", file);
+	}
+
+	private static org.opengis.feature.simple.SimpleFeature addPoint( org.opengis.feature.simple.SimpleFeatureType type, double lat, double lng, String name )
+	{
+		org.geotools.feature.simple.SimpleFeatureBuilder simpleFeatureBuilder = new org.geotools.feature.simple.SimpleFeatureBuilder( type );
+		org.locationtech.jts.geom.Coordinate coordinate = new org.locationtech.jts.geom.Coordinate( lat, lng );
+		org.locationtech.jts.geom.Point point = gf.createPoint( coordinate );
+		simpleFeatureBuilder.add( point );
+		simpleFeatureBuilder.add( name );
+		org.opengis.feature.simple.SimpleFeature simpleFeature = simpleFeatureBuilder.buildFeature( name );
+
+		org.geotools.process.spatialstatistics.core.SpatialEvent spatialEvent = new org.geotools.process.spatialstatistics.core.SpatialEvent( name, coordinate );
+		events.add( spatialEvent );
+
+		return simpleFeature;
 	}
 
 	private static SimpleFeatureSource getSource(String fileName) throws Exception {
