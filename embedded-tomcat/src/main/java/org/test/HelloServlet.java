2f8500f4d5238b500d277d8137f93c6219fe4143
Stephen More <stephen.more@gmail.com>
2020-09-21 12:20:19 -0400

    Adding sample embedded tomcat.

---
+++ embedded-tomcat/src/main/java/org/test/HelloServlet.java
@@ -0,0 +1,16 @@
+package org.test;
+
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet( name = "MyServlet", urlPatterns = {"/hello"} )
+public class HelloServlet extends javax.servlet.http.HttpServlet
+{
+	@Override
+	protected void doGet(javax.servlet.http.HttpServletRequest req, javax.servlet.http.HttpServletResponse resp) throws javax.servlet.ServletException, java.io.IOException {
+        javax.servlet.ServletOutputStream out = resp.getOutputStream();
+        out.write( "oh hello there".getBytes());
+        out.flush();
+        out.close();
+    }
+
+}
---
+++ embedded-tomcat/src/main/java/org/test/Main.java
@@ -0,0 +1,34 @@
+package org.test;
+
+public class Main
+{
+
+	public static void main(String[] args) throws Exception
+	{
+		String webappDirLocation = "src/main/webapp/";
+		org.apache.catalina.startup.Tomcat tomcat = new org.apache.catalina.startup.Tomcat();
+
+		//The port that we should run on can be set into an environment variable
+		//Look for that variable and default to 8080 if it isn't there.
+		String webPort = System.getenv("PORT");
+		if(webPort == null || webPort.isEmpty())
+		{ 
+			webPort = "8080";
+		}
+
+		tomcat.setPort(Integer.valueOf(webPort));
+
+		org.apache.catalina.core.StandardContext ctx = (org.apache.catalina.core.StandardContext)tomcat.addWebapp("/", new java.io.File(webappDirLocation).getAbsolutePath());
+		System.out.println("configuring app with basedir: " + new java.io.File("./" + webappDirLocation).getAbsolutePath());
+
+		// Declare an alternative location for your "WEB-INF/classes" dir
+		// Servlet 3.0 annotation will work
+		java.io.File additionWebInfClasses = new java.io.File("target/classes");
+		org.apache.catalina.WebResourceRoot resources = new org.apache.catalina.webresources.StandardRoot(ctx);
+		resources.addPreResources(new org.apache.catalina.webresources.DirResourceSet(resources, "/WEB-INF/classes", additionWebInfClasses.getAbsolutePath(), "/"));
+		ctx.setResources(resources);
+
+		tomcat.start();
+		tomcat.getServer().await();
+	}
+}
