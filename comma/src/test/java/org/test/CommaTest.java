bf17ed49cdafb9d9089fabe9c388a2ddb9217850
Stephen More <stephen.more@gmail.com>
2020-11-25 17:49:40 -0500

    Also testing with apache commons.

--- comma/src/test/java/org/test/CommaTest.java
+++ comma/src/test/java/org/test/CommaTest.java
@@ -9,7 +9,7 @@ public class CommaTest
 	@Test
 	public void testOpenCsv() throws Exception
 	{
-		log.info( "OpenCsv" );
+		log.info( "\nOpenCsv" );
 
 		com.opencsv.CSVReader reader = new com.opencsv.CSVReader( new java.io.FileReader("src/test/resources/sample.data") );
 
@@ -23,7 +23,7 @@ public class CommaTest
 	@Test
 	public void testSpark() throws Exception
 	{
-		log.info( "spark" );
+		log.info( "\nspark" );
 
 		org.apache.spark.sql.SparkSession spark = org.apache.spark.sql.SparkSession.builder().appName("Simple Application").config("spark.master", "local").getOrCreate();
 		org.apache.spark.sql.SQLContext sqlContext = new org.apache.spark.sql.SQLContext( spark.sparkContext() );
@@ -39,6 +39,22 @@ public class CommaTest
 		for( org.apache.spark.sql.Row row : rows )
 		{
 			log.info( row.getString( 0 ) + "\t" + row.getString( 2 ) + "\t" + row.getString( 1 ) );
+		}
+	}
+
+	@Test
+	public void testCommons() throws Exception
+	{
+		log.info( "\ncommons" );
+
+		java.io.Reader in = new java.io.FileReader("src/test/resources/sample.data");
+		Iterable<org.apache.commons.csv.CSVRecord> records = org.apache.commons.csv.CSVFormat.DEFAULT
+			.withHeader( "h1","h2","h3" )
+			.withFirstRecordAsHeader()
+			.parse(in);
+		for( org.apache.commons.csv.CSVRecord record : records )
+		{
+        		log.info( record.get( "h1" ) + "\t" + record.get( "h3" ) + "\t" + record.get( "h2" ) );
 		}
 	}
 }
