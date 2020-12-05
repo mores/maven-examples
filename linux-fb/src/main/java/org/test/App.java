19737fc4242acc91af5b4aabc150bae5e27df6af
Stephen More <stephen.more@gmail.com>
2020-12-05 10:24:56 -0500

    Now able to open and create a file.

--- linux-fb/src/main/java/org/test/App.java
+++ linux-fb/src/main/java/org/test/App.java
@@ -18,6 +18,27 @@ public class App
 
         public void run() throws Exception
         {
-		CLibrary.INSTANCE.printf( "Hello, World\n" );
+		String plainJavaString =  "Hello, World\n";
+		CLibrary.INSTANCE.printf( plainJavaString );
+
+		int fd = 0;
+		try
+		{
+			fd = CLibrary.INSTANCE.open( "/tmp/abc", CLibrary.O_RDWR | CLibrary.O_CREAT );
+
+			byte[] data = plainJavaString.getBytes( java.nio.charset.StandardCharsets.UTF_8 );
+			CLibrary.INSTANCE.write( fd, data, data.length );
+		}
+		catch( Exception e )
+                {
+                        java.io.StringWriter sw = new java.io.StringWriter();
+                        java.io.PrintWriter pw = new java.io.PrintWriter( sw );
+                        e.printStackTrace( pw );
+                        log.error( "Error:" + sw.toString() );
+                }
+		finally
+		{
+			CLibrary.INSTANCE.close( fd );
+		}
 	}
 }
--- linux-fb/src/main/java/org/test/CLibrary.java
+++ linux-fb/src/main/java/org/test/CLibrary.java
@@ -2,7 +2,18 @@ package org.test;
 
 public interface CLibrary extends com.sun.jna.Library
 {
+	public static final int O_RDONLY = 0;
+	public static final int O_WRONLY = 1;
+	public static final int O_RDWR = 2;
+	public static final int O_CREAT = 100;
+
 	CLibrary INSTANCE = (CLibrary)com.sun.jna.Native.load( (com.sun.jna.Platform.isWindows() ? "msvcrt" : "c"), CLibrary.class );
 
+	public int close( int fd );
+
+	public int open( String path, int flags );
+
 	public void printf( String format, Object... args );
+
+	public int write( int fd, byte[] data, int size );
 }
