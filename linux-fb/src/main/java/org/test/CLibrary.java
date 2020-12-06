f783d14c35a93e881205993fb5cd478bc773137a
Stephen More <stephen.more@gmail.com>
2020-12-05 21:31:50 -0500

    Adding ioctl to CLibrary

--- linux-fb/src/main/java/org/test/CLibrary.java
+++ linux-fb/src/main/java/org/test/CLibrary.java
@@ -2,6 +2,13 @@ package org.test;
 
 public interface CLibrary extends com.sun.jna.Library
 {
+	public static final int FBIOGET_VSCREENINFO = 0x4600;
+	public static final int FBIOPUT_VSCREENINFO = 0x4601;
+	public static final int FBIOGET_FSCREENINFO = 0x4602;
+	public static final int FBIOGETCMAP = 0x4604;
+	public static final int FBIOPUTCMAP = 0x4605;
+	public static final int FBIOPAN_DISPLAY = 0x4606;
+
 	public static final int O_RDONLY = 0;
 	public static final int O_WRONLY = 1;
 	public static final int O_RDWR = 2;
@@ -10,6 +17,8 @@ public interface CLibrary extends com.sun.jna.Library
 	CLibrary INSTANCE = (CLibrary)com.sun.jna.Native.load( (com.sun.jna.Platform.isWindows() ? "msvcrt" : "c"), CLibrary.class );
 
 	public int close( int fd );
+
+	public void ioctl( int fd, int cmd, ScreenInfo data ) throws com.sun.jna.LastErrorException;
 
 	public int open( String path, int flags );
 
