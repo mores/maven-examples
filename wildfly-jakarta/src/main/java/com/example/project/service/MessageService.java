9e65abecc9e2be169024e6fe6aefc626e24af59f
Stephen More <stephen.more@gmail.com>
2021-10-16 16:18:57 -0400

    Returns the appropriate EntityManager.

--- wildfly-jakarta/src/main/java/com/example/project/service/MessageService.java
+++ wildfly-jakarta/src/main/java/com/example/project/service/MessageService.java
@@ -4,21 +4,37 @@ import java.util.List;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceUnit;
 
 import com.example.project.model.Message;
 
 @Stateless
 public class MessageService {
 
-	@PersistenceContext
-	private EntityManager entityManager;
+	@PersistenceContext(unitName = "EvenPersistenceUnit")
+	private EntityManager entityManagerEven;
 
-	public void create(Message message) {
-		entityManager.persist(message);
+	@PersistenceContext(unitName = "OddPersistenceUnit")
+	private EntityManager entityManagerOdd;
+
+	private EntityManager getEntityManager(String multitenancyIdentifier) {
+
+		if ("even".equals(multitenancyIdentifier)) {
+			return entityManagerEven;
+		} else if ("odd".equals(multitenancyIdentifier)) {
+			return entityManagerOdd;
+		}
+
+		return entityManagerEven;
 	}
 
-	public List<Message> list() {
-		return entityManager.createQuery("FROM Message m", Message.class).getResultList();
+	public void create(String multitenancyIdentifier, Message message) {
+		getEntityManager(multitenancyIdentifier).persist(message);
+	}
+
+	public List<Message> list(String multitenancyIdentifier) {
+		return getEntityManager(multitenancyIdentifier).createQuery("FROM Message m", Message.class).getResultList();
 	}
 }
