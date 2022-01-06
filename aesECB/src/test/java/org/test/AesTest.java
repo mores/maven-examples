cd56354fd744961fb02cc822aea772c863966346
mores <stephen.more@gmail.com>
2022-01-06 18:56:24 +0000

    Adds a CMAC.

--- aesECB/src/test/java/org/test/AesTest.java
+++ aesECB/src/test/java/org/test/AesTest.java
@@ -7,7 +7,6 @@ public class AesTest
         private static org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger( AesTest.class );
 
 	private javax.crypto.Cipher c;
-	private javax.crypto.SecretKey myKey;
 	private byte[] encodedKey;
 
         @Test
@@ -16,26 +15,37 @@ public class AesTest
 		log.info( "testOne" );
 
 		c = javax.crypto.Cipher.getInstance( "AES/ECB/PKCS5Padding" );
-		myKey = javax.crypto.KeyGenerator.getInstance( "AES" ).generateKey();
 
-		encodedKey = myKey.getEncoded();
-		System.out.println( org.apache.commons.codec.binary.Hex.encodeHexString( encodedKey ) );
+		encodedKey = hexStringToByteArray( "b685645b2fb26c4a83360efa35bd4727" );
+		log.info( "Key: " + org.apache.commons.codec.binary.Hex.encodeHexString( encodedKey ) );
 
 		String original = "This data in encrypted using Java AES ECB";
+		byte[] text = original.getBytes();
 
-		byte[] str = performEncryption( original );
-		System.out.println( "Encrypted String : " + org.apache.commons.codec.binary.Hex.encodeHexString( str ) );
+		byte[] str = performEncryption( text );
+		log.info( "Encrypted String : " + org.apache.commons.codec.binary.Hex.encodeHexString( str ) );
 
 		String decryptedString = performDecryption( str );
-		System.out.println( "Decrypted String : " + decryptedString );
+		log.info( "Decrypted String : " + decryptedString );
 
 		org.junit.Assert.assertEquals( original, decryptedString );
+
+		org.bouncycastle.crypto.BlockCipher cipher = new org.bouncycastle.crypto.engines.AESEngine();
+		org.bouncycastle.crypto.macs.CMac cmac = new org.bouncycastle.crypto.macs.CMac( cipher );
+		cmac.init( new org.bouncycastle.crypto.params.KeyParameter( encodedKey ) );
+		cmac.update( text, 0, text.length );
+		byte[] out = new byte[cmac.getMacSize()];
+		cmac.doFinal( out, 0 );
+		//byte[] cipherMessageAuthCode = java.util.Arrays.copyOf(out, lengthBits / 8);
+
+		log.info( "CMAC: " + org.apache.commons.codec.binary.Hex.encodeHexString( out ) );
 	}
 
-	public byte[] performEncryption(String s) throws Exception
+	public byte[] performEncryption( byte[] text ) throws Exception
 	{
+		javax.crypto.SecretKey myKey = new javax.crypto.spec.SecretKeySpec( encodedKey, 0, encodedKey.length, "AES" );
+
 		c.init( javax.crypto.Cipher.ENCRYPT_MODE, myKey );
-		byte[] text = s.getBytes();
 		byte[] textEncrypted = c.doFinal( text );
 		return( textEncrypted );
 	}
@@ -47,5 +57,16 @@ public class AesTest
 		c.init( javax.crypto.Cipher.DECRYPT_MODE, originalKey );
 		byte[] textDecrypted = c.doFinal( s );
 		return( new String( textDecrypted ) );
+	}
+
+	public static byte[] hexStringToByteArray( String s )
+	{
+		int len = s.length();
+		byte[] data = new byte[len / 2];
+		for( int i = 0; i < len; i += 2 )
+		{
+			data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i+1), 16));
+		}
+		return data;
 	}
 }
