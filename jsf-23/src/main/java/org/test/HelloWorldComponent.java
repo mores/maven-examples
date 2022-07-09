068349a6b9b027a0723590b1fa05f3aab3b26681
Stephen More <stephen.more@gmail.com>
2022-07-09 07:43:46 -0400

    Example now shows how the link inside the component is not working.

--- jsf-23/src/main/java/org/test/HelloWorldComponent.java
+++ jsf-23/src/main/java/org/test/HelloWorldComponent.java
@@ -5,6 +5,8 @@ import javax.faces.component.FacesComponent;
 @FacesComponent(tagName = "helloWorldComponent", createTag = true, namespace = "http://test.org/component")
 public class HelloWorldComponent extends javax.faces.component.UIComponentBase
 {
+	private static org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger( HelloWorldComponent.class );
+
 	@Override
 	public String getFamily()
 	{
@@ -14,7 +16,28 @@ public class HelloWorldComponent extends javax.faces.component.UIComponentBase
 	@Override
 	public void encodeBegin( javax.faces.context.FacesContext context) throws java.io.IOException
 	{
+		javax.faces.application.Application application = context.getApplication();
+                javax.el.ExpressionFactory ef = application.getExpressionFactory();
+
 		javax.faces.context.ResponseWriter writer = context.getResponseWriter();
 		writer.write("Hello World!");
+
+		// Issue number 1 - why is this null
+		javax.el.ValueExpression vex = getValueExpression( "value" );
+		log.error( "VEX: " + vex );
+
+		org.primefaces.component.panel.Panel panel = new org.primefaces.component.panel.Panel();
+		panel.setHeader( "Component" );
+		panel.setFooter( "footer" );
+		panel.setParent( this );
+
+		org.primefaces.component.commandlink.CommandLink page2 = new org.primefaces.component.commandlink.CommandLink();
+		page2.setActionExpression( ef.createMethodExpression( context.getELContext(), "page2.jsf", null, new Class[0] ) );
+		page2.setAjax( false );
+		page2.setValue( "Page 2" );
+		panel.getChildren().add( page2 );
+
+		panel.encodeBegin( context );
+		panel.encodeEnd( context );
 	}
 }
