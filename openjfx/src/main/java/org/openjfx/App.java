3037b34cff5d5e5e9cc86112a60525e28f58fe9a
Stephen More <stephen.more@gmail.com>
2023-07-03 10:00:41 -0400

    Adds a menu item.

--- openjfx/src/main/java/org/openjfx/App.java
+++ openjfx/src/main/java/org/openjfx/App.java
@@ -13,12 +13,32 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
+
         var javaVersion = SystemInfo.javaVersion();
         var javafxVersion = SystemInfo.javafxVersion();
 
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
-        stage.setScene(scene);
+	javafx.scene.layout.BorderPane borderPane = new javafx.scene.layout.BorderPane();
+
+	javafx.scene.control.MenuBar menuBar = new javafx.scene.control.MenuBar();
+	borderPane.setTop( menuBar );
+
+	javafx.scene.control.Menu file = new javafx.scene.control.Menu( "File" );
+	menuBar.getMenus().add( file );
+
+	javafx.scene.control.MenuItem quit = new javafx.scene.control.MenuItem( "Quit" );
+	file.getItems().add( quit );
+
+	quit.setOnAction(e -> {
+		System.out.println("Quit Selected");
+		System.exit( 0 );
+	});
+
+        javafx.scene.control.Label label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        borderPane.setCenter( new StackPane(label) );
+
+	javafx.scene.Scene scene = new javafx.scene.Scene( borderPane, 640, 480 );
+        stage.setScene( scene );
+	stage.setTitle("Hello World!!");
         stage.show();
     }
 
