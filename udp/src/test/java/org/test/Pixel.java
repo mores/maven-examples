17888fc7dc19fc13583c344f1548682630785d7d
Stephen More <smore@Stephens-Mini.localdomain>
2023-07-05 15:42:44 -0400

    Now able to go the full brightness.

--- udp/src/test/java/org/test/Pixel.java
+++ udp/src/test/java/org/test/Pixel.java
@@ -1,106 +1,152 @@
 package org.test;
 
+/*
+	Java byte 8-bit signed -128 -> 127
+	we want to work with 0 -> 255
+*/
+
 public class Pixel
 {
-	private byte red;
-	private byte green;
-	private byte blue;
+	private int red;
+	private int green;
+	private int blue;
 
 	public Pixel()
 	{
-		red = 0x7F;
-		green = 0x7F;
-		blue = 0x7F;
+		red = 255;
+		green = 255;
+		blue = 255;
 	}
 
-	public byte getRed()
+	public int getRed()
 	{
 		return red;
 	}
 
-	public void setRed( byte red )
+	public void setRed( int red )
 	{
 		this.red = red;
 	}
 
-	public byte getGreen()
+	public int getGreen()
         {
                 return green;
         }
 
-        public void setGreen( byte green )
+        public void setGreen( int green )
         {
                 this.green = green;
         }
 
-	public byte getBlue()
+	public int getBlue()
         {
                 return blue;
         }
 
-        public void setBlue( byte blue )
+        public void setBlue( int blue )
         {
                 this.blue = blue;
         }
 
 	// binary
-
 	public void on()
         {
-                red = 0x7F;
-                green = 0x7F;
-                blue = 0x7F;
+                red = 255;
+                green = 255;
+                blue = 255;
         }
 
         public void off()
         {
-                red = 0x00;
-                green = 0x00;
-                blue = 0x00;
+                red = 0;
+                green = 0;
+                blue = 0;
         }
 
 	// colors
+	public void azure()
+	{
+		red = 0;
+		green = 165;
+		blue = 255;
+	}
 
 	public void blue()
 	{
-		red = 0x00;
-                green = 0x00;
-                blue = 0x7F;
+		red = 0;
+                green = 0;
+                blue = 255;
+	}
+
+	public void chartreuse()
+	{
+		red = 165;
+		green = 255;
+		blue = 0;
 	}
 
 	public void cyan()
 	{
-		red = 0x00;
-                green = 0x7F;
-                blue = 0x7F;
+		red = 0;
+                green = 255;
+                blue = 255;
 	}
 
 	public void green()
         {
-                red = 0x00;
-                green = 0x7F;
-                blue = 0x00;
+                red = 0;
+                green = 255;
+                blue = 0;
         }
 
 	public void magenta()
 	{
-		red = 0x7F;
-		green = 0x00;
-                blue = 0x7F;
+		red = 255;
+		green = 0;
+                blue = 255;
+	}
+
+	public void orange()
+	{
+		// 100% red + 64.7% green
+		red = 255;
+                green = 165;
+                blue = 0;
+	}
+
+	public void pink()
+	{
+		red = 255;
+		green = 0;
+		blue = 165;
 	}
 
 	public void red()
 	{
-		red = 0x7F;
-		green = 0x00;
-                blue = 0x00;
+		red = 255;
+		green = 0;
+                blue = 0;
+	}
+
+	public void springGreen()
+	{
+		red = 0;
+		green = 255;
+		blue = 165;
+	}
+
+	public void violet()
+	{
+		red = 165;
+		green = 0;
+		blue = 255;
 	}
 
 	public void yellow()
 	{
-		red = 0x7F;
-		green = 0x7F;
-		blue = 0x00;
+		red = 255;
+		green = 255;
+		blue = 0;
 	}
 
 	//
@@ -108,9 +154,9 @@ public class Pixel
 	public byte[] getRGB()
 	{
 		byte[] data = new byte[3];
-		data[0] = red;
-		data[1] = green;
-		data[2] = blue;
+		data[0] = (byte)red;
+		data[1] = (byte)green;
+		data[2] = (byte)blue;
 
 		return data;
 	}
