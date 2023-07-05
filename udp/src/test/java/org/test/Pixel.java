14e6c25587f0978365d9e97aa7af07c1c9e1d856
Stephen More <smore@Stephens-Mini.localdomain>
2023-07-05 14:49:37 -0400

    Adding in more colors.

--- udp/src/test/java/org/test/Pixel.java
+++ udp/src/test/java/org/test/Pixel.java
@@ -43,6 +43,68 @@ public class Pixel
                 this.blue = blue;
         }
 
+	// binary
+
+	public void on()
+        {
+                red = 0x7F;
+                green = 0x7F;
+                blue = 0x7F;
+        }
+
+        public void off()
+        {
+                red = 0x00;
+                green = 0x00;
+                blue = 0x00;
+        }
+
+	// colors
+
+	public void blue()
+	{
+		red = 0x00;
+                green = 0x00;
+                blue = 0x7F;
+	}
+
+	public void cyan()
+	{
+		red = 0x00;
+                green = 0x7F;
+                blue = 0x7F;
+	}
+
+	public void green()
+        {
+                red = 0x00;
+                green = 0x7F;
+                blue = 0x00;
+        }
+
+	public void magenta()
+	{
+		red = 0x7F;
+		green = 0x00;
+                blue = 0x7F;
+	}
+
+	public void red()
+	{
+		red = 0x7F;
+		green = 0x00;
+                blue = 0x00;
+	}
+
+	public void yellow()
+	{
+		red = 0x7F;
+		green = 0x7F;
+		blue = 0x00;
+	}
+
+	//
+
 	public byte[] getRGB()
 	{
 		byte[] data = new byte[3];
