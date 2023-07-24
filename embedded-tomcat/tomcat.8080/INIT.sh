b9f7b87ff62d6f2ba0128aa9ab587eff8a7acc41
Stephen More <stephen.more@gmail.com>
2023-07-24 07:55:22 -0400

    Also exports out a PEM.

--- embedded-tomcat/tomcat.8080/INIT.sh
+++ embedded-tomcat/tomcat.8080/INIT.sh
@@ -5,3 +5,6 @@ echo "Enter hostname: "
 read hostname
 
 keytool -genkey -keyalg RSA -alias selfsigned -keystore keystore.jks -storepass password -validity 360 -keysize 2048 -dname "CN=$hostname, OU=Unknown, O=Unknown, L=Unknown, ST=Unknown, C=Unknown" -ext SAN=dns:$hostname
+
+## The following can export a PEM file
+keytool -exportcert -rfc -alias selfsigned -file $hostname.pem -keystore keystore.jks -storepass password -v
