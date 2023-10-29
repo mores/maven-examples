e46a0ec8eac4d0484351ccf0b7240cc3c5eb4ac1
Stephen More <smore@Stephens-Mini.localdomain>
2023-10-29 11:48:29 -0400

    Renaming.

--- tplink/src/test/java/org/test/LightState.java
+++ tplink/src/test/java/org/test/LightState.java
@@ -1,10 +1,10 @@
 package org.test;
 
-public class Bulb
+public class LightState 
 {
 	private Transition transition_light_state;
 
-	public Bulb( Pixel pixel )
+	public LightState( Pixel pixel )
 	{
 		transition_light_state = new Transition( pixel );
 	}
