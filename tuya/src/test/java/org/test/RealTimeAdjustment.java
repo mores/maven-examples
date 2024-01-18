67964131885dbf1dc307e4971de37219cc2bc093
Stephen More <smore@Stephens-Mac-mini.local>
2024-01-18 12:12:40 -0500

    Able to fully customize the color of this fixture.

--- tuya/src/test/java/org/test/RealTimeAdjustment.java
+++ tuya/src/test/java/org/test/RealTimeAdjustment.java
@@ -2,12 +2,42 @@ package org.test;
 
 public class RealTimeAdjustment implements DataPoint
 {
+	private static org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger( TuyaTest.class );
+
+	// RGB bulb
+	private int hue; // 0 - 360
+	private int saturation; // 0 - 1000
+	private int value; // 0 - 1000
+
+	// White bulb
+	private int brightness; // 0 - 1000
+	private int temperature; // 0 - 1000
+
+	public RealTimeAdjustment( int brightness, int temperature )
+	{
+		this.brightness = brightness;
+		this.temperature = temperature;
+	}
+
+	public RealTimeAdjustment( int red, int green, int blue )
+	{
+		float[] hsb = java.awt.Color.RGBtoHSB( red, green, blue, null );
+		this.hue = (int)(hsb[0] * 360);
+		this.saturation = (int)(hsb[1] * 1000);
+		this.value = (int)(hsb[2] * 1000);
+
+		log.info( "Hue: " + hue );
+		log.info( "Saturation: " + saturation );
+		log.info( "Value: " + value );
+	}
+
 	@Override
 	public java.util.Map<String,String> getCommand()
 	{
 		java.util.Map<String, String> dps = new java.util.HashMap<>();
-		//dps.put( "28", "103e803e803e800120025" );
-		dps.put( "28", "1007603e803e800120025" );
+
+		// gradiant
+		dps.put( "28", "1" + String.format("%04x", hue ) + String.format("%04x", saturation ) + String.format("%04x", value ) + String.format("%04x", brightness ) + String.format("%04x", temperature ) );
 
 		return dps;
 	}	
