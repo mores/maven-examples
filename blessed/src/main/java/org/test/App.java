6669635c4251678338dfe4b7dfd56d03a9e9dad3
Stephen More <stephen.more@gmail.com>
2024-03-26 11:17:59 -0400

    More advanced with writing back to server.

--- blessed/src/main/java/org/test/App.java
+++ blessed/src/main/java/org/test/App.java
@@ -7,15 +7,19 @@ public class App
 	private static org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger( App.class );
 
 	// _ENV_SENSE_UUID = bluetooth.UUID(0x181A)
-	private static java.util.UUID SERVICE = java.util.UUID.fromString( "0000181a-0000-1000-8000-00805f9b34fb" );
+	private static java.util.UUID ENV_SERVICE = java.util.UUID.fromString( "0000181a-0000-1000-8000-00805f9b34fb" );
 
 	// _TEMP_CHAR = bluetooth.UUID(0x2A6E)
 	private static java.util.UUID TEMP = java.util.UUID.fromString( "00002a6e-0000-1000-8000-00805f9b34fb" );
+
+	private static java.util.UUID TIME_SERVICE = java.util.UUID.fromString("00001805-0000-1000-8000-00805f9b34fb" );
+	private static java.util.UUID TIME = java.util.UUID.fromString( "00002a2b-0000-1000-8000-00805f9b34fb" );
 
 	private final String[] args;
 	private String lookingFor;
 
 	private com.welie.blessed.BluetoothCentralManager central;
+	private com.welie.blessed.BluetoothPeripheral connectedPeripheral;
 	private static final java.util.Set<String> knownDevices = new java.util.HashSet<>();
 
 	public static void main( String[] args )
@@ -31,6 +35,24 @@ public class App
 		{
 			this.lookingFor = args[0];
 		}
+
+		Runtime.getRuntime().addShutdownHook(new Thread()
+		{
+			public void run()
+			{
+				try
+				{
+					System.out.println("Shutting down ...");
+					central.cancelConnection( connectedPeripheral );
+					Thread.sleep(200);
+				}
+				catch( InterruptedException e)
+				{
+					Thread.currentThread().interrupt();
+					e.printStackTrace();
+				}
+			}
+		});
         }
 
 	public void run()
@@ -52,7 +74,21 @@ public class App
 				log.info( "\tDiscovered Service: " + service.getUuid() );
 			}
 
-			peripheral.setNotify( SERVICE, TEMP, true );
+			peripheral.setNotify( ENV_SERVICE, TEMP, true );
+
+			com.welie.blessed.BluetoothGattCharacteristic writeCharacteristic = peripheral.getCharacteristic(TIME_SERVICE, TIME);
+			if( writeCharacteristic != null )
+			{
+				peripheral.setNotify(writeCharacteristic, true);
+
+				if( writeCharacteristic.supportsWritingWithResponse()) {
+
+					com.welie.blessed.BluetoothBytesParser parser = new com.welie.blessed.BluetoothBytesParser();
+					parser.setCurrentTime( java.util.Calendar.getInstance() );
+
+					peripheral.writeCharacteristic( writeCharacteristic, parser.getValue(), com.welie.blessed.BluetoothGattCharacteristic.WriteType.WITH_RESPONSE);
+				}
+			}
 		}
 
 		@Override
@@ -61,10 +97,21 @@ public class App
 			String output = org.apache.commons.codec.binary.Hex.encodeHexString( value );
 			log.info( "onCharacteristicUpdate: " + output );
 
-			final com.welie.blessed.BluetoothBytesParser parser = new com.welie.blessed.BluetoothBytesParser( value );	
-			int rawTemp = parser.getIntValue( com.welie.blessed.BluetoothBytesParser.FORMAT_UINT16, 0, java.nio.ByteOrder.LITTLE_ENDIAN );
-			java.math.BigDecimal tempDegreeC = new java.math.BigDecimal( rawTemp ).divide( new java.math.BigDecimal( "100" ), 2, java.math.RoundingMode.HALF_UP );
-			log.info( "temp: " + tempDegreeC + " degc " );
+			final com.welie.blessed.BluetoothBytesParser parser = new com.welie.blessed.BluetoothBytesParser( value );
+			if( characteristic.getUuid().equals( TEMP ))
+			{
+				int rawTemp = parser.getIntValue( com.welie.blessed.BluetoothBytesParser.FORMAT_UINT16, 0, java.nio.ByteOrder.LITTLE_ENDIAN );
+				java.math.BigDecimal tempDegreeC = new java.math.BigDecimal( rawTemp ).divide( new java.math.BigDecimal( "100" ), 2, java.math.RoundingMode.HALF_UP );
+				log.info( "\ttemp: " + tempDegreeC + " degc " );
+			}
+			else if( characteristic.getUuid().equals( TIME ))
+			{
+				log.info( "\ttime: " + parser.getDateTime() );
+			}
+			else
+			{
+				log.info( "Unknown Characteristic: " + characteristic.getUuid() );
+			}
 		}
 	};
 
@@ -84,6 +131,7 @@ public class App
 			{
 				central.stopScan();
 				central.connectPeripheral( peripheral, peripheralCallback );
+				connectedPeripheral = peripheral;
 			}
 		}
 	};
