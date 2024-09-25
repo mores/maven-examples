3a86cbdee6f395f2e859360891214acbc8a331c1
Stephen More <stephen.more@gmail.com>
2024-09-25 12:30:58 -0400

    We only need to work with segments.

--- geometry/src/test/java/org/test/LineTest.java
+++ geometry/src/test/java/org/test/LineTest.java
@@ -21,30 +21,18 @@ public class LineTest
                 log.info( "lineTest" );
 		Precision.DoubleEquivalence precision = Precision.doubleEquivalenceOfEpsilon(1e-6);
 		
-		Line linea = Lines.fromPoints(Vector2D.ZERO, Vector2D.of(0, 75), precision);
-		log.info( "Line A: " + linea );
-
-		Segment segmenta = linea.segment( 0.0, 75.0 );
+		Segment segmenta = Lines.segmentFromPoints(Vector2D.ZERO, Vector2D.of(0, 75), precision);
 		log.info( "Segment A: " + segmenta );
-
 		splitSegment( segmenta, 15 );
 
 		//
-	        Line lineb = Lines.fromPoints(Vector2D.ZERO, Vector2D.of(75, 0), precision);
-                log.info( "Line B: " + lineb );
-
-                Segment segmentb = lineb.segment( 0.0, 75.0 );
+	        Segment segmentb = Lines.segmentFromPoints(Vector2D.ZERO, Vector2D.of(75, 0), precision);
                 log.info( "Segment B: " + segmentb );
-
 		splitSegment( segmentb, 15 );
 
 		//
-		Line linec = Lines.fromPoints(Vector2D.ZERO, Vector2D.of(75, 75), precision);
-                log.info( "Line C: " + linec );
-
-                Segment segmentc = linec.segment( 0.0, 75.0 );
+		Segment segmentc = Lines.segmentFromPoints(Vector2D.ZERO, Vector2D.of(75, 75), precision);
                 log.info( "Segment C: " + segmentc );
-
                 splitSegment( segmentc, 15 );
 	}
 
