9a2b9b03da87d3987946336f54f160bf872e738b
Stephen More <stephen.more@gmail.com>
2025-03-18 16:54:45 -0400

    Fixes test.

--- daylightSavings/src/test/java/org/test/AppTest.java
+++ daylightSavings/src/test/java/org/test/AppTest.java
@@ -9,6 +9,11 @@ public class AppTest
 	@Test
 	public void testIt() throws Exception
 	{
+		log.info( " - - - - - - - - - - - - - - - - - - - - - - - ");
+		log.info( "Vendor: " + System.getProperty("java.vendor" ) );
+		log.info( "Version: " + System.getProperty("java.version") );
+		log.info( "Runtime: " + System.getProperty("java.runtime.version") );
+
 		log.info( "Time: "+  createTimes( 2025, 0, 10, 6, 15, 0 ) );
 		log.info( "Time: "+  createTimes( 2025, 1, 10, 6, 15, 0 ) );
 		log.info( "Time: "+  createTimes( 2025, 2, 10, 6, 15, 0 ) );
