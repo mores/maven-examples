edec224c47df2e889104b3a956bd172b90b4eac9
Stephen More <smore@Stephens-Mini.localdomain>
2025-05-21 14:50:51 -0400

    Now opens when jlinked.

--- openjfx/src/main/java/module-info.java
+++ openjfx/src/main/java/module-info.java
@@ -1,9 +1,13 @@
 module hellofx {
+
     requires javafx.controls;
     requires javafx.fxml;
     requires org.slf4j;
     requires org.apache.logging.log4j;
     requires org.apache.logging.log4j.core;
     requires org.apache.logging.log4j.slf4j2.impl;
+
     exports org.openjfx;
+
+    opens org.openjfx;
 }
--- openjfx/src/main/java/org/openjfx/FxmlEvent.java
+++ openjfx/src/main/java/org/openjfx/FxmlEvent.java
@@ -2,6 +2,8 @@ package org.openjfx;
 
 public class FxmlEvent implements javafx.event.EventHandler<javafx.event.ActionEvent> {
 
+	private static org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(FxmlEvent.class);
+
 	private Model model;
 
 	public FxmlEvent(Model model) {
@@ -13,6 +15,7 @@ public class FxmlEvent implements javafx.event.EventHandler<javafx.event.ActionE
 
 		javafx.fxml.FXMLLoader loader = new javafx.fxml.FXMLLoader();
 		java.net.URL xmlUrl = getClass().getResource("/scene.fxml");
+		log.info("URL: " + xmlUrl);
 		loader.setLocation(xmlUrl);
 		loader.setControllerFactory(new javafx.util.Callback<Class<?>, Object>() {
 			@Override
@@ -27,8 +30,11 @@ public class FxmlEvent implements javafx.event.EventHandler<javafx.event.ActionE
 		try {
 			javafx.scene.Parent root = loader.load();
 			newWindow.setScene(new javafx.scene.Scene(root));
-		} catch (Exception e) {
-			e.printStackTrace();
+		} catch (Exception exception) {
+			java.io.StringWriter sw = new java.io.StringWriter();
+			java.io.PrintWriter pw = new java.io.PrintWriter(sw);
+			exception.printStackTrace(pw);
+			log.error(exception.getMessage() + " " + sw.toString());
 		}
 
 		newWindow.initOwner(model.getStage());
